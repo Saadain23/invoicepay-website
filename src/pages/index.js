30cf778a77c44d7b12b54e93fed24bd1847afc5b
Saadain23 <saadaink23@gmail.com>
2025-03-03 02:15:18 +0200

    add pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
-import Image from "next/image";
 import Hero from "@/components/Hero";
 import Testimonials from "@/components/Testimonials";
 import Pricing from "@/components/Pricing";
 import Features from "@/components/Features";
 import FAQ from "@/components/FAQ";
 import CTA from "@/components/CTA";
+import Script from 'next/script';
 
 
 export default function Home() {
@@ -13,8 +13,29 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Home</title>
-        <meta name="description" content="Home" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>InvoicePay - Simple invoicing solution for Light Entrepreneurs | InvoicePay</title>
+        <meta name="description" content="InvoicePay is a simple invoicing solution for freelancers and entrepreneurs. Handle invoicing, mileage claims 2024 and accounting hassle-free. Start for free (free invoicing) today!" />
+        <meta name="keywords" content="invoicing, invoicing solution, freelancer, entrepreneur, accounting, free invoicing, mileage claims, accounting hassle-free" />
+
+        <meta property="og:title" content="InvoicePay - Simple invoicing solution for your business"/>
+        <meta property="og:description" content="Simple and reliable invoicing solution for freelancers and entrepreneurs" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://invoicepay.fi" />
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YR399ZZPE3"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YR399ZZPE3');
+          `}
+        </Script>
       </Head>
       <Hero />
       <Testimonials />

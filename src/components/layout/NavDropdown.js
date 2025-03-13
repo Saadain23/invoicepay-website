d15b2f59b481552143cf9902e719eeaa0f7fb453
Azeezulla Khan <seetherisingazeez@live.com>
2025-03-13 19:40:31 +0530

    fixed nav dropdown on mobile.

--- src/components/layout/NavDropdown.js
+++ src/components/layout/NavDropdown.js
@@ -3,16 +3,16 @@ import styles from '@/styles/NavDropdown.module.css';
 const NavDropdown = ({ currentLang, translations }) => {
   return (
     <div className={styles.dropdown}>
-      <a href={`/${currentLang}/#services`} className={styles.dropdownTrigger}>
+      <a className={styles.dropdownTrigger}>
         {translations.Navbar?.services}
-        <svg 
-          xmlns="http://www.w3.org/2000/svg" 
-          width="20" 
-          height="20" 
-          viewBox="0 0 24 24" 
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
           className={styles.dropdownArrow}
         >
-          <path d="M7 10l5 5 5-5z"/>
+          <path d="M7 10l5 5 5-5z" />
         </svg>
       </a>
       <div className={styles.dropdownContent}>

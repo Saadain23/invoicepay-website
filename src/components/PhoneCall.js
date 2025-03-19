1f54241192e7753d96957278ecb84ed376f9a0a2
Azeezulla Khan <seetherisingazeez@live.com>
2025-03-19 18:23:54 +0530

    fixed phone call input field

--- src/components/PhoneCall.js
+++ src/components/PhoneCall.js
@@ -9,7 +9,6 @@ class PhoneCall extends Component {
                     type="tel"
                     className={styles.phoneInput}
                     placeholder="+358123456789"
-                    value="+358123456789"
                 />
                 <select className={styles.languageSelect}>
                     <option value="en">English</option>
